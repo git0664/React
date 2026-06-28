745971d9aacf8bd82b61370cbb6c521262c5bf96
RAJ TRIPATHI <tripathiraj6406@gmail.com>
2026-06-28 11:33:39 +0530

    add RTE file

--- MiniBlogApp/src/components/index.js
+++ MiniBlogApp/src/components/index.js
@@ -3,11 +3,13 @@ import Footer from "./footer/Footer.jsx"
 import Container from './container/Container.jsx'
 import Logo from './Logo.jsx'
 import LogoutBtn from "./header/LogoutBtn.jsx"
+import RTE from './RTE.jsx'
 
 export{
     Header, 
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE
 }
